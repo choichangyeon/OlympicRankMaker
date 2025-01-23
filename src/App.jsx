0bc09104aa9665d97dd37e3814f8b6cd4fe83c5f
choichangyeon <changyon99@naver.com>
2025-01-23 16:52:45 +0900

    refact:#15-Table 컴포넌트 이동 및 state 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,77 @@
-import React from 'react'
-import Title from './Title';
-import InputForm from './InputForm';
-
-const mainStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '5px',
-};
+import { useState } from "react";
+import Title from "./Title";
+import InputForm from "./InputForm";
+import Table from "./Table";
 
 const App = () => {
+  //국가 목록 state
+  let initial = JSON.parse(window.localStorage.getItem("countries"));
+  const [countries, setCountries] = useState(initial || []);
+
+  //정렬 기준 state
+  const [head, setHead] = useState("국가");
+
+  //table head 목록
+  const heads = ["국가", "금메달", "은메달", "동메달"];
 
   return (
     <>
       <main style={mainStyle}>
         <Title />
-        <InputForm />
-      </main >
+        <InputForm
+          countries={countries}
+          setCountries={setCountries}
+          heads={heads}
+          head={head}
+          setHead={setHead}
+        />
+        <Table
+          countries={sortByHead(countries, head) ?? countries}
+          setCountries={setCountries}
+          heads={heads}
+        ></Table>
+      </main>
     </>
-  )
-}
+  );
+};
+//List 형태의 컴포너트도 있으면 좋겠다 / 분리 필요
+//state를 이곳에 생성..
 
-export default App
+//기준으로 정렬 함수
+const sortByHead = (countries, head) => {
+  let sortCountries = [...countries];
+  switch (
+    head //set 하지말고 그냥 소팅된걸 넘긴다. - 완료
+  ) {
+    case "금메달":
+      sortCountries.sort((a, b) => {
+        return b.gold - a.gold;
+      });
+      return sortCountries;
+    case "은메달":
+      sortCountries.sort((a, b) => {
+        return b.sliver - a.sliver;
+      });
+      return sortCountries;
+    case "동메달":
+      sortCountries.sort((a, b) => {
+        return b.bronze - a.bronze;
+      });
+      return sortCountries;
+    default:
+      sortCountries.sort((a, b) => {
+        return a.country.localeCompare(b.country);
+      });
+      return sortCountries;
+  }
+};
+
+const mainStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "5px",
+};
+
+export default App;
--- src/Submit.jsx
+++ src/Submit.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 //사용자 제출 버튼 컴포넌트
 const Submit = ({ value, name }) => {
-    const submitStyle = {
-      display: 'flex',
-      width: '80px'
-    };
-    return <input type='submit' style={submitStyle} value={value} name={name} />;
-  }
+  //이름 다시 설정
+  const submitStyle = {
+    display: "flex",
+    width: "80px",
+  };
+  return <input type="submit" style={submitStyle} value={value} name={name} />;
+};
 
-export default Submit
+export default Submit;
