8274501f3534d8210df5cb1a75aa47dc591c45b8
choichangyeon <changyon99@naver.com>
2025-01-24 10:15:07 +0900

    refact:#15-상수 변환

--- src/DropDown.jsx
+++ src/DropDown.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 //드롭다운 컴포넌트
-const DropDown = ({ heads, setHead, state }) => {
+const DropDown = ({ heads, setValue, state }) => {
   //필터 값 설정 함수
-  const headSelect = (e) => {
-    setHead(e.target.value);
+  const valueSelect = (e) => {
+    setValue(e.target.value);
   };
 
   return (
-    <select onChange={headSelect} value={state}>
+    <select onChange={valueSelect} value={state}>
       {heads.map((head) => {
         return <option key={head}>{head}</option>;
       })}
